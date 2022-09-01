cab906fe6d508b164d6e81e4b6ef4df1beb23ce2
Datagate <datagate_service@example.com>
2022-09-01 17:45:05 +0800

    test

--- index.js
+++ index.js
@@ -2,5 +2,18 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const exec = require('@actions/exec');
 
+let myOutput = '';
+let myError = '';
 
-await exec.exec('node', ['index.js', 'foo=bar']);
+const options = {};
+options.listeners = {
+  stdout: (data: Buffer) => {
+    myOutput += data.toString();
+  },
+  stderr: (data: Buffer) => {
+    myError += data.toString();
+  }
+};
+options.cwd = './lib';
+
+await exec.exec('node', ['index.js', 'foo=bar'], options);
