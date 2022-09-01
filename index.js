b5221625fe4e58be88689f7294fdc80abb4e9cc5
Datagate <datagate_service@example.com>
2022-09-01 17:58:55 +0800

    fff

--- index.js
+++ index.js
@@ -4,9 +4,24 @@ const exec = require('@actions/exec');
 
 
 async function main() {
+
+    let myOutput = '';
+    let myError = '';
+
+    const options = {};
+    options.listeners = {
+    stdout: (data: Buffer) => {
+        myOutput += data.toString();
+    },
+    stderr: (data: Buffer) => {
+        myError += data.toString();
+    }
+    };
+    options.cwd = './lib';
+
     await core.group('install pre-commit', async () => {
         // await exec.exec('pip', ['install', 'pre-commit']);
-        await exec.exec('ls', []);
+        await exec.exec('ls', [], options);
     });
 
 }
