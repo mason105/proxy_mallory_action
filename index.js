0315a61a1c43ac2d2a6ac1d21607aa0a1ecc505a
Datagate <datagate_service@example.com>
2022-09-01 17:30:24 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const exec = require('@actions/exec');
+let myOutput = '';
+let myError = '';
+
+
+const options = {
+listeners: {
+stdout: (data: Buffer) => {
+myOutput += data.toString();
+},
+stderr: (data: Buffer) => {
+myError += data.toString();
+}
+}
+};
+
+
+await exec.exec('git', ['rev-parse', '--short', 'HEAD'], options);
