ac28c21da03ba38b6eae0b3c300572c622f81a5d
Datagate <datagate_service@example.com>
2022-09-01 17:49:08 +0800

    tttt

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const exec = require('@actions/exec');
 
 async function main() {
     await core.group('install pre-commit', async () => {
-        await exec.exec('pip', ['install', 'pre-commit']);
+        // await exec.exec('pip', ['install', 'pre-commit']);
         await exec.exec('node', ['index.js', 'foo=bar']);
     });
 
