330dcaa9591cd12dbed9996bc50b21674ed859b2
Datagate <datagate_service@example.com>
2022-09-01 17:49:00 +0800

    ttt

--- index.js
+++ index.js
@@ -2,18 +2,13 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const exec = require('@actions/exec');
 
-let myOutput = '';
-let myError = '';
 
-const options = {};
-options.listeners = {
-  stdout: (data: Buffer) => {
-    myOutput += data.toString();
-  },
-  stderr: (data: Buffer) => {
-    myError += data.toString();
-  }
-};
-options.cwd = './lib';
+async function main() {
+    await core.group('install pre-commit', async () => {
+        await exec.exec('pip', ['install', 'pre-commit']);
+        await exec.exec('node', ['index.js', 'foo=bar']);
+    });
 
-await exec.exec('node', ['index.js', 'foo=bar'], options);
+}
+
+main().catch((e) => core.setFailed(e.message));
