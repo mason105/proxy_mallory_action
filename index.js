ffd5b50a134d35d4408d143fe49b815074547041
Datagate <datagate_service@example.com>
2022-09-01 17:40:32 +0800

    t3

--- index.js
+++ index.js
@@ -2,18 +2,5 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const exec = require('@actions/exec');
 
-let myOutput = '';
-let myError = '';
 
-const options = {};
-options.listeners = {
-  stdout: (data: Buffer) => {
-    myOutput += data.toString();
-  },
-  stderr: (data: Buffer) => {
-    myError += data.toString();
-  }
-};
-
-
-await exec.exec('git', ['rev-parse', '--short', 'HEAD'], options);
+await exec.exec('node', ['index.js', 'foo=bar']);
